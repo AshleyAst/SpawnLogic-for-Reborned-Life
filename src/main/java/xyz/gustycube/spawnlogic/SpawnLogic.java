dc2abec334a259ed6b5e9705f58f5e86733a5dcc
AshleyAst <164010909+AshleyAst@users.noreply.github.com>
2024-06-15 10:13:59 +0000

    Patches High Ping Issue

--- src/main/java/xyz/gustycube/spawnlogic/SpawnLogic.java
+++ src/main/java/xyz/gustycube/spawnlogic/SpawnLogic.java
@@ -1,26 +1,38 @@
 package xyz.gustycube.spawnlogic;
 
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.inventory.Inventory;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
-import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.Arrays;
+import redempt.redlib.config.ConfigManager;
+import redempt.redlib.config.annotations.ConfigMappable;
+import redempt.redlib.config.annotations.ConfigName;
+
+import org.bukkit.configuration.file.FileConfiguration;
 import java.util.logging.Level;
 
 public final class SpawnLogic extends JavaPlugin implements Listener {
     public static SpawnLogic instance;
+
+    FileConfiguration config = getConfig();
+
+    @ConfigMappable
+    public class cls {
+        @ConfigName("CSME (Custom Spawn Message Enabler) Configuration Settings")
+        public boolean CSME = false;
+        public String CSM = "This is an Default CSME Message on Player Join. Please set it to your own.";
+
+        public boolean VPS = true;
+    }
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        ConfigManager config = ConfigManager.create(this).target(SpawnLogic.class).saveDefaults().load();
+
+        /** PLUGIN BASIC STARTUP */
         instance = this;
         this.getLogger().log(Level.INFO, "SpawnLogic Enabled");
         getServer().getPluginManager().registerEvents(this, this);
@@ -30,7 +42,7 @@ public final class SpawnLogic extends JavaPlugin implements Listener {
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        /** PLUGIN BASIC SHUTDOWN */
         this.getLogger().log(Level.INFO, "SpawnLogic Disabled");
 
     }
@@ -40,38 +52,62 @@ public final class SpawnLogic extends JavaPlugin implements Listener {
         Bukkit.getScheduler().runTaskTimer(SpawnLogic.instance, () -> { } , 0, 60L);
         int playerPing = e.getPlayer().getPing();
         Player player = e.getPlayer();
-        if (playerPing > 200) {
-            Bukkit.getScheduler().runTask(this, () -> {
-                Inventory inv = Bukkit.createInventory(player, 27, ChatColor.RED.toString() + ChatColor.BOLD + "You have high ping!");
-                // LEAVE BUTTON
-                ItemStack leave = new ItemStack(Material.RED_DYE);
-                ItemMeta meta = leave.getItemMeta();
-                meta.setDisplayName(ChatColor.RED + "Leave the game");
-                meta.setLore(Arrays.asList("You should rejoin with a VPN!"));
-                leave.setItemMeta(meta);
+        boolean CSME = config.getBoolean("CSME");
+        String CSM = config.getString("CSM");
+        boolean VPS = config.getBoolean("VPS");
+        
+        if (CSME == true) {
+            player.sendMessage(CSM);
+        }
 
-                inv.setItem(12, leave);
-
-                // CONTINUE
-                ItemStack continueButton = new ItemStack(Material.GREEN_DYE);
-                ItemMeta continueMeta = leave.getItemMeta();
-                continueMeta.setDisplayName(ChatColor.RED + "Continue");
-                continueMeta.setLore(Arrays.asList("You may experience lag, world generation glitches, and more!"));
-                continueButton.setItemMeta(continueMeta);
-
-                inv.setItem(14, continueButton);
-
-                // BACKGROUND
-                ItemStack background = new ItemStack(Material.GRAY_STAINED_GLASS_PANE);
-                ItemMeta backgroundMeta = background.getItemMeta();
-                backgroundMeta.setDisplayName("\n");
-                background.setItemMeta(backgroundMeta);
-
-                for (int i : new int[]{0,1,2,3,4,5,6,7,8,9,10,11,13,15,16,17,18,19,20,21,22,23,24,25,26}) {
-                    inv.setItem(i, background);
-                }
-                player.openInventory(inv);
-            });
+        if (VPS == true) {
+            if (playerPing > 200) {
+                player.sendMessage("Unstable Connection to RBL Network. For more Information, please visit rebornedlife.github.io/docs");
+    
+                /**
+                 * PATCHES HIGH PING ISSUE
+                 * 
+                 * This code basicially creates an GUI and runs
+                 * it at the start of the player join. Which causes
+                 * alot of lag, so for In testing we will disable
+                 * it to see if anything improves. If so it will
+                 * be permantly disabled.
+                 * 
+                 * Bukkit.getScheduler().runTask(this, () -> {
+                    Inventory inv = Bukkit.createInventory(player, 27, ChatColor.RED.toString() + ChatColor.BOLD + "You have high ping!");
+                    // LEAVE BUTTON
+                    ItemStack leave = new ItemStack(Material.RED_DYE);
+                    ItemMeta meta = leave.getItemMeta();
+                    meta.setDisplayName(ChatColor.RED + "Leave the game");
+                    meta.setLore(Arrays.asList("You should rejoin with a VPN!"));
+                    leave.setItemMeta(meta);
+    
+                    inv.setItem(12, leave);
+    
+                    // CONTINUE
+                    ItemStack continueButton = new ItemStack(Material.GREEN_DYE);
+                    ItemMeta continueMeta = leave.getItemMeta();
+                    continueMeta.setDisplayName(ChatColor.RED + "Continue");
+                    continueMeta.setLore(Arrays.asList("You may experience lag, world generation glitches, and more!"));
+                    continueButton.setItemMeta(continueMeta);
+    
+                    inv.setItem(14, continueButton);
+    
+                    // BACKGROUND
+                    ItemStack background = new ItemStack(Material.GRAY_STAINED_GLASS_PANE);
+                    ItemMeta backgroundMeta = background.getItemMeta();
+                    backgroundMeta.setDisplayName("\n");
+                    background.setItemMeta(backgroundMeta);
+    
+                    for (int i : new int[]{0,1,2,3,4,5,6,7,8,9,10,11,13,15,16,17,18,19,20,21,22,23,24,25,26}) {
+                        inv.setItem(i, background);
+                    }
+                    player.openInventory(inv);
+                });
+                 * 
+                 */
+                
+            }
         }
     }
 
